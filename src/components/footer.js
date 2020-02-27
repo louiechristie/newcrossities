5ec21a4938280bb9cc4b6bc008db2b960ff2df49
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2020-02-27 20:39:15 +0000

    Add call to action

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,28 +2,38 @@ import React from "react"
 import { Link } from "gatsby"
 
 const Footer = () => (
-  <footer>
-    <p>
-      Newcrossities is a weary travellers’ guide to the new curiosities of South
-      London, New Cross, Deptford, and beyond:
+  <>
+    <footer>
+      <p>
+        Newcrossities is a weary travellers’ guide to the new curiosities of
+        South London, New Cross, Deptford, and beyond:
+      </p>
+      <ul>
+        <li>Curated and opinionated.</li>
+        <li>
+          <strong>Underground, weird and funny.</strong>
+        </li>
+        <li>Completely incomplete</li>
+      </ul>
+      © {new Date().getFullYear()} Newcrossities -{` `}
+      <Link
+        to="/about/"
+        style={{
+          color: `white`,
+        }}
+      >
+        About
+      </Link>
+    </footer>
+
+    <p class="cta">
+      <a href="https://blog.louiechristie.com/contact/">
+        Want website like this one? *
+      </a>
+      <br />
+      <small>(*other colors/styles are available)</small>
     </p>
-    <ul>
-      <li>Curated and opinionated.</li>
-      <li>
-        <strong>Underground, weird and funny.</strong>
-      </li>
-      <li>Completely incomplete</li>
-    </ul>
-    © {new Date().getFullYear()} Newcrossities -{` `}
-    <Link
-      to="/about/"
-      style={{
-        color: `white`,
-      }}
-    >
-      About
-    </Link>
-  </footer>
+  </>
 )
 
 export default Footer
