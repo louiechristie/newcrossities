5a143da9f296f111ca5bb1e69ddd4b008f336c71
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2020-06-05 00:17:29 +0100

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,7 @@ module.exports = {
     },
     "gatsby-plugin-instagram-embed",
     "gatsby-plugin-twitter",
+    "gatsby-plugin-sitemap",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
